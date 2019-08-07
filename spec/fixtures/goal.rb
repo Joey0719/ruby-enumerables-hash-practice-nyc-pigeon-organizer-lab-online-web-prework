8e2886f209ae3de6258170e45cb7e971a9f55b9f
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-07 20:28:43 +0000

    Automatically backed up by Learn

--- spec/fixtures/goal.rb
+++ spec/fixtures/goal.rb
@@ -1,37 +1,37 @@
 {
   "Theo" => {
-    ":color => ["purple", "grey"],
-    :gender => ["male"],
+    ":color => ['purple', 'grey'],
+    :gender => ['male'],
     :lives => ['Subway']
   },
-  "Peter Jr." => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Library"]
+  'Peter Jr.' => {
+    :color => ['purple', 'grey'],
+    :gender => ['male'],
+    :lives => ['Library']
   },
-  "Lucky" => {
-    :color => ["purple"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
+  'Lucky' => {
+    :color => ['purple'],
+    :gender => ['male'],
+    :lives => ['Central Park']
   },
-  "Ms .K" => {
-    :color => ["grey", "white"],
-    :gender => ["female"],
-    :lives => ["Central Park"]
+  'Ms .K' => {
+    :color => ['grey', 'white'],
+    :gender => ['female'],
+    :lives => ['Central Park']
   },
-  "Queenie" => {
-    :color => ["white", "brown"],
-    :gender => ["female"],
-    :lives => ["Subway"]
+  'Queenie' => {
+    :color => ['white', 'brown'],
+    :gender => ['female'],
+    :lives => ['Subway']
   },
-  "Andrew" => {
-    :color => ["white"],
-    :gender => ["male"],
-    :lives => ["City Hall"]
+  'Andrew' => {
+    :color => ['white'],
+    :gender => ['male'],
+    :lives => ['City Hall']
   },
-  "Alex" => {
-    :color => ["white", "brown"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
+  'Alex' => {
+    :color => ['white', 'brown'],
+    :gender => ['male'],
+    :lives => ['Central Park']
   }
 }
